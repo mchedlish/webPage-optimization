54e8b33554bbc73dfd2089f55e5bfa8ba75347ab
Nikoloz Mchedlishvili <mchedlish@yahoo.com>
2017-06-30 17:30:27 +0400

    function updatePositions changed

--- views/js/main.js
+++ views/js/main.js
@@ -491,10 +491,10 @@ function logAverageFrame(times) { // times is the array of User Timing measureme
 function updatePositions() {
     frame++;
     window.performance.mark("mark_start_frame");
-
+    var outOfLoop = document.body.scrollTop / 1250;
     var items = document.querySelectorAll('.mover');
     for (var i = 0; i < items.length; i++) {
-        var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+        var phase = Math.sin((outOfLoop) + (i % 5));
         items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
     }
 
