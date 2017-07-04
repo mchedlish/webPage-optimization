df61d82185f3694b43bcc816e83777b0a74257d5
mchedlish <mchedlish@yahoo.com>
2017-07-04 20:46:02 +0400

    submission 4

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+
+gulp.task('compress', function (cb) {
+  pump([
+        gulp.src('views/js/main.js'),
+        uglify(),
+        gulp.dest('views/js/mainmin.js')
+    ],
+    cb
+  );
+});
